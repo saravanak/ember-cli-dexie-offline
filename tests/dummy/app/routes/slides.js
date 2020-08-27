124741768e62300d9f7ab05b80303dd4b6dcf369
Saravana Kumar V(Saro) <vsk62.gate@gmail.com>
2020-08-27 15:14:27 +0530

    Cache the slides and re-init them on next run

--- tests/dummy/app/routes/slides.js
+++ tests/dummy/app/routes/slides.js
@@ -1,9 +1,32 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class SlidesRoute extends Route {
   queryParams = {
-    slides: {
-      replace: true
+    slide: {
+      replace: true,
+      refreshModel: true
     }
-  };
+  }
+
+  @service dexieOffline;
+
+  async beforeModel(transition) {
+    console.log(transition.to.queryParams);
+    const { slide } = transition.to.queryParams;
+    if(slide) {
+      await this.dexieOffline.db._slides.put({
+        id: 1,
+        slide
+      })
+    } else {
+      const savedSlide = await this.dexieOffline.db._slides.get(1);
+      if(savedSlide) {
+        const slide = Number(savedSlide.slide) || 0 ;
+        this.transitionTo({
+          queryParams: {slide}
+        })
+      }
+    }
+  }
 }
