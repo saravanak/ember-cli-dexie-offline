7c2ff4836d8c6c853ea6f064f444519eb8b54ac8
Saravana Kumar V(Saro) <vsk62.gate@gmail.com>
2020-08-27 15:14:51 +0530

    Refine the online status indicator

--- tests/dummy/app/components/online-status.js
+++ tests/dummy/app/components/online-status.js
@@ -1,21 +1,29 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class OnlineStatusComponent extends Component {
+  @tracked status;
+  @tracked isOnline;
+
   @action init() {
     //Taken from MDN: https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/Online_and_offline_events
     var status = document.getElementById('status');
 
+    const me = this;
+
     function updateOnlineStatus() {
       var condition = navigator.onLine ? 'online' : 'offline';
-      console.log('inside handler');
-
-      status.className = condition;
-      status.innerHTML = condition.toUpperCase();
+      me.isOnline = navigator.onLine;
+      me.status = condition.toUpperCase();
     }
 
     window.addEventListener('online', updateOnlineStatus);
     window.addEventListener('offline', updateOnlineStatus);
     updateOnlineStatus(window.navigator.onLine);
+  }
+
+  @action refreshWindow(){
+    location.reload();
   }
 }
