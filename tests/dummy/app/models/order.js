f9ab68070d6c5361a24bbb9772beb90380c0a11e
Saravana Kumar V(Saro) <vsk62.gate@gmail.com>
2020-09-11 14:03:17 +0530

    Add more details.

--- tests/dummy/app/models/order.js
+++ tests/dummy/app/models/order.js
@@ -3,6 +3,7 @@ import Model, { belongsTo, attr } from '@ember-data/model';
 export default class OrderBookModel extends Model {
   @belongsTo product;
   @attr cost;
+  @attr quantity;
   @attr isBumper;
   @attr('boolean') isCreatedOffline;
   @attr({ dexieIndex: true }) status;
